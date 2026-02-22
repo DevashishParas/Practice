38788717ad4f2a5b541e4f9594570a1d288db0fa
devashish.web@gmail.com <devashish.web@gmail.com>
2026-02-22 18:04:47 +0530

    prac

--- T_pattern3_4.cpp
+++ T_pattern3_4.cpp
@@ -17,3 +17,24 @@ int main() {
    }
  return 0;
 }
+
+
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//    int n;
+//    cin>>n;
+
+//    for(int i=0; i<n; i++){
+   
+//     for(int j=1; j<=i+1; j++){
+
+//         cout<<j<<" ";
+
+//     }
+//     cout<<endl;
+
+//    }
+//  return 0;
+// }
--- T_rev_pattern_3_5.cpp
+++ T_rev_pattern_3_5.cpp
@@ -5,7 +5,7 @@ int main() {
   cin>>n;
 
   for(int i=0; i<n; i++){
-    for(int j=i+1; j>0; j++){
+    for(int j=i+1; j>0; j--){
 
         cout<<j<<" ";
 
