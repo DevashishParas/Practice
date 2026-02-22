f0d4e453ade833e8dbdb0332c72d2f0f61f85104
devashish.web@gmail.com <devashish.web@gmail.com>
2026-02-22 08:44:09 +0530

    prac

--- T_pattern3_3.cpp
+++ T_pattern3_3.cpp
@@ -2,21 +2,20 @@
 using namespace std;
 int main() {
   
+
     int n;
     cin>>n;
     char ch='A';
-    
     for(int i=0; i<n; i++){
-       
-        
+
         for(int j=0; j<i+1; j++){
 
             cout<<ch<<" ";
-          
-          
+
         }
         cout<<endl;
-           ch=ch+1;
+        ch=ch+1;
+
     }
  return 0;
 }
