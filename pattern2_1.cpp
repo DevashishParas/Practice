a6ec5dfff45e5147f7bce577663b7191300ed6db
devashish.web@gmail.com <devashish.web@gmail.com>
2026-02-22 07:53:14 +0530

    practice

--- pattern2_1.cpp
+++ pattern2_1.cpp
@@ -9,7 +9,7 @@ int main()
 
    for(int i=0; i<n; i++){
     for(int j=0; j<n; j++){
-        cout<<num;
+        cout<<num<<" ";
         num++;
 
     }
--- temp.cpp
+++ temp.cpp
@@ -14,5 +14,6 @@ int main() {
     cout<<endl;
 
    }
+   cout<<"after pattern "<< num;
  return 0;
 }
