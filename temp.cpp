18bcecae8aaa5a1a06ffb7fa3273ec9d7659116e
devashish.web@gmail.com <devashish.web@gmail.com>
2026-02-22 08:26:54 +0530

    prac

--- temp.cpp
+++ temp.cpp
@@ -4,11 +4,12 @@ int main() {
   
     int n;
     cin>>n;
+    char ch='A';
     for(int i=0; i<n; i++){
         for(int j=0; j<i+1; j++){
 
-            cout<<i+1<<" ";
-            
+            cout<<ch<<" ";
+            ch=ch+1;
         }
         cout<<endl;
     }
