fee2331b6f6e59b9afcd712276ec0946c3c79e22
devashish.web@gmail.com <devashish.web@gmail.com>
2026-02-22 07:47:34 +0530

    practice

--- temp.cpp
+++ temp.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    int n ;
-    
-    cin>>n;
-        
-    for(int i=0; i<n; i++){
-       
-         char ch='A';
+int main() {
+  int n;
+  int num=1;
+  cin>>n;
+   for(int i=0; i<n; i++){
 
-        for(int j=0; j<n; j++){
-            cout<<ch<<" ";
-            ch++;
-        }
-        cout<<endl;
+    for(int j=0; j<n; j++){
+
+        cout<<num<<" ";
+        num++;
     }
+    cout<<endl;
+
+   }
  return 0;
 }
